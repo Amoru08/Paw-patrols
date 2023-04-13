2422566daf74cefe705a2bb25fd4a039c6c062af
Amor <amore.acecalrgmail.com>
2023-04-13 12:04:46 +0200

    1 Commit

---
+++ Profesion.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Profesion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Profesion
+{
+   // instance variables - replace the example below with your own
+    private String nombreProfesion;
+    
+    private String rango;
+    /**
+     * Constructor for objects of class Profesion
+     */
+    public Profesion(String Profesion, String Rango)
+    {
+     nombreProfesion = Profesion;
+     rango = Rango;
+    }
+}
