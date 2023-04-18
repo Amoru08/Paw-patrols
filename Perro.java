aad779df8a754a6374732875d3b0f3411387e1a6
Mauro <maurogutierreztormo@gmail.com>
2023-04-18 12:11:50 +0200

    4 commit

---
+++ Perro.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Perro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Perro
+{
+    private String nombrePerro;
+    private String raza;
+    private Profesion profesion;
+}
